3de8154e170bba1c1851ce0511c6e615d294429c
vivek <vivek.niec94@gmail.com>
2024-02-25 21:42:34 +0530

    add new line for addition

--- Java Testing/Index.java
+++ Java Testing/Index.java
@@ -2,5 +2,8 @@ class Demo{
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is java programming");
+        int a= 10, b= 20;
+        int c= a+b;
+        System.out.println(c);
     }
 }
